d9e4cd370b3595e4100019da52d3e396b2bdc8c0
asuvorov <artem.suvorov@gmail.com>
2011-09-26 22:01:51 +0300

    Update #3328

--- django_fabfile/tests/test_backup.py
+++ django_fabfile/tests/test_backup.py
@@ -1,55 +1,53 @@
-import fudge
-import unittest
-from django_fabfile.backup import backup_instance, trim_snapshots, rsync_snapshot
-#import datetime
-#import utils
-#import boto
-#from boto import connect_ec2
-#from utils import get_region_conn
-
-#from boto.ec2.instance import Instance
-#from boto.ec2.blockdevicemapping import BlockDeviceMapping
+from django.utils import unittest
 from boto.sqs import regions
-import random, string
 
+import fudge
+import random
+import string
+#import datetime
+
+from django_fabfile.backup import backup_instance, trim_snapshots
+from django_fabfile.backup import rsync_snapshot
 
 # Specifying the test package
 test_pkg = 'django_fabfile.backup.'
 
 
-#------------------------------------------------------------------------------
-# Some useful functions
-#------------------------------------------------------------------------------
-# Generator of unique IDs for snapshots, volumes, etc.
 def key_gen(chars):
-    return ''.join(random.sample(string.ascii_letters + string.digits, chars))
+    """
+    Generator of unique IDs for snapshots, volumes, etc.
+    """
+    return ''.join(random.sample(string.ascii_lowercase + string.digits, chars))
 
 
 #------------------------------------------------------------------------------
 # Fake classes to isolate test method from outer space
 # TODO replace following fakes to another file
 #------------------------------------------------------------------------------
+
+
 class Connection():
+    """
+    Fake - replacement for class 'boto.connection.Connection'
+    """
+
     def __init__(self, region_name):
         print '>>> Connection.__init__({0})'.format(region_name)
         self.region = region_name
         return
-    
 
     def get_all_volumes(self, vol_id):
         """
-        Fake - replacement for Connection.get_all_volumes()
+        Fake - replacement for 'Connection.get_all_volumes()'
         """
-        #_ret_val = vol_id
-        _ret_val = ['vol-'+key_gen(8), 'vol-'+key_gen(8)]
+        _ret_val = ['vol-' + key_gen(8), 'vol-' + key_gen(8)]
         print '>>> Connection.get_all_volumes({0})'.format(vol_id)
         print '... return {0}'.format(_ret_val)
         return _ret_val
 
-
     def get_all_regions(self):
         """
-        Fake - replacement for Connection.get_all_regions
+        Fake - replacement for 'Connection.get_all_regions'
         Returns list of AWS regions, using regular method 'boto.sqs.regions()'
         """
         _ret_val = regions()
@@ -57,41 +55,45 @@ class Connection():
         print '... return {0}'.format(_ret_val)
         return _ret_val
 
-
     def get_all_snapshots(self, snapshot_ids=None, owner=None,
         restorable_by=None, filters=None):
         """
-        Fake - replacement for Connection.get_all_snapshots
+        Fake - replacement for 'Connection.get_all_snapshots'
         """
-        #_ret_val = fudge.Fake('Snapshot').provides('tags').returns(fudge.Fake('tags').has_attr(get='name'))
-        #_ret_val = fudge.Fake('Snapshot').provides('tags').returns_fake().provides('get').returns(fudge.Fake('get').has_attr(name='name'))
-        #_ret_val = 'Snapshot'
         _ret_val = Snapshot(self.region)
-        print '>>> Connection.get_all_snapshots({0}, {1}, {2}, {3})'.format(snapshot_ids, owner, restorable_by, filters)
+        print '>>> Connection.get_all_snapshots({0}, {1}, {2}, {3})' \
+            .format(snapshot_ids, owner, restorable_by, filters)
         print '... return {0}'.format(_ret_val)
-        return [_ret_val,]
+        return [_ret_val, ]
 
 
 class Instance():
     """
     Fake - replacement for class 'boto.ec2.instance.Instance'
     """
+
     def __init__(self):
         print '>>> Instance.__init__()'
-        self.block_device_mapping = {'dev-1': fudge.Fake('dev-1').has_attr(volume_id=['vol-11', 'vol-12']),
-            'dev-2': fudge.Fake('dev-2').has_attr(volume_id=['vol-21', 'vol-22'])}
-        return    
+        self.block_device_mapping = {
+            'dev-1': fudge.Fake('dev-1').has_attr(
+                volume_id=['vol-11', 'vol-12']),
+            'dev-2': fudge.Fake('dev-2').has_attr(
+                volume_id=['vol-21', 'vol-22'])}
+        return
 
 
 class Tags():
-    #TODO fill up doc-stirng
+    """
+    Fake - replacement for class 'boto.ec2.tag.Tags'
+    """
+
     def __init__(self):
         print '>>> Tags.__init__()'
         return
 
     def get(self, name=None):
         if name == 'Name':
-            _ret_val = 'fake:Name'
+            _ret_val = 'fake:Name-' + key_gen(8)
         else:
             _ret_val = None
             pass
@@ -99,8 +101,12 @@ class Tags():
         print '... return {0}'.format(_ret_val)
         return _ret_val
 
+
 class Snapshot():
-    #TODO fill up doc-stirng
+    """
+    Fake - replacement for class 'boto.ec2.snapshot.Snapshot'
+    """
+
     def __init__(self, region):
         print '>>> Snapshot.__init__()'
         self.tags = Tags()
@@ -119,7 +125,7 @@ class Snapshot():
 
 def get_region_conn(region_name=None):
     """
-    Fake - replacement for utils.get_region_conn()
+    Fake - replacement for 'utils.get_region_conn()'
     """
     conn = Connection(region_name)
     print '>>> get_region_conn({0})'.format(region_name)
@@ -129,20 +135,21 @@ def get_region_conn(region_name=None):
 
 def get_inst_by_id(region_name, instance_id):
     """
-    Fake - replacement for utils.get_inst_by_id()
+    Fake - replacement for 'utils.get_inst_by_id()'
     """
     instance = Instance()
     print '>>> get_inst_by_id({0}, {1})'.format(region_name, instance_id)
     print '... instance = {0}'.format(instance)
-    print '... instance.block_device_mapping = {0}'.format(instance.block_device_mapping)
+    print '... instance.block_device_mapping = {0}' \
+        .format(instance.block_device_mapping)
     return instance
 
 
 def create_snapshot(vol, synchronously):
     """
-    Fake - replacement for backup.create_snapshot
+    Fake - replacement for 'backup.create_snapshot'
     """
-    _ret_val = fudge.Fake('snap-'+key_gen(8))
+    _ret_val = fudge.Fake('snap-' + key_gen(8))
     print '>>> create_snapshot({0}, {1})'.format(vol, synchronously)
     print '... return {0}'.format(_ret_val)
     return _ret_val
@@ -150,7 +157,7 @@ def create_snapshot(vol, synchronously):
 
 def delete_broken_snapshots():
     """
-    Fake - replacement for backup.delete_broken_snapshots()
+    Fake - replacement for 'backup.delete_broken_snapshots()'
     """
     print '>>> delete_broken_snapshots()'
     return
@@ -158,7 +165,7 @@ def delete_broken_snapshots():
 
 def _trim_snapshots(reg, dry_run):
     """
-    Fake - replacement for backup._trim_snapshots()
+    Fake - replacement for 'backup._trim_snapshots()'
     """
     print '>>> _trim_snapshots({0}, {1})'.format(reg, dry_run)
     return
@@ -166,10 +173,10 @@ def _trim_snapshots(reg, dry_run):
 
 def get_snap_device(snap):
     """
-    Fake - replacement for utils.get_snap_device()
+    Fake - replacement for 'utils.get_snap_device()'
     """
-    _ret_val = '/dev/sda'
-    #_ret_val = '/dev/sdb'
+    #_ret_val = '/dev/sda'
+    _ret_val = '/dev/sd' + key_gen(1)
     print '>>> get_snap_device({0})'.format(snap)
     print '... return {0}'.format(_ret_val)
     return _ret_val
@@ -178,12 +185,12 @@ def get_snap_device(snap):
 #------------------------------------------------------------------------------
 # Testing tasks
 #------------------------------------------------------------------------------
-class TestBackup(unittest.TestCase):
-    def setUp(self):
-        return
 
-    @fudge.patch(test_pkg+'get_region_conn', test_pkg+'get_inst_by_id',
-        test_pkg+'create_snapshot')
+
+class TestBackup(unittest.TestCase):
+
+    @fudge.patch(test_pkg + 'get_region_conn', test_pkg + 'get_inst_by_id',
+        test_pkg + 'create_snapshot')
     def test_backup_instance(self, fakeMethod1, fakeMethod2, fakeMethod3):
         fakeMethod1.is_callable().calls(get_region_conn)
         fakeMethod2.is_callable().calls(get_inst_by_id)
@@ -193,78 +200,63 @@ class TestBackup(unittest.TestCase):
 
         # Test case #1
         print "\nTEST 1 - backup.backup_instance(region_name)"
-        try: backup_instance('us-east-1')
-        except Exception, e: print 'STOP: ', e
-
         self.assertRaises(Exception, backup_instance, 'us-east-1')
 
         # Test case #2
         print "\nTEST 2 - backup.backup_instance(region_name, instance_id)"
-        try: print backup_instance('us-east-1', 'i-12345678')
-        except Exception, e: print 'STOP: ', e
+        self.assertIsInstance(backup_instance('us-east-1', 'i-12345678'),
+            list, 'Oops, we have catched an assertion error')
 
         # Test case #3
         #TODO work out with such test issue
         print "\nTEST 3 - backup.backup_instance(region_name, instance)"
-        try: print backup_instance('us-east-1', instance)
-        except Exception, e: print 'STOP: ', e
+        self.assertIsInstance(backup_instance('us-east-1', instance), list,
+            'Oops, we have catched an assertion error')
 
         # Test case #4
-        print "\nTEST 4 - backup.backup_instance(region_name, instance_id, synchronously)"
-        try: print backup_instance('us-east-1', 'i-12345678', synchronously=True)
-        except Exception, e: print 'STOP: ', e
+        print "\nTEST 4 - backup.backup_instance(region_name," \
+            " instance_id, synchronously)"
+        self.assertIsInstance(backup_instance('us-east-1', 'i-12345678',
+            synchronously=True), list,
+            'Oops, we have catched an assertion error')
 
         # Test case #5
-        print "\nTEST 5 - backup.backup_instance(region_name, instance_id, instance)"
-        try: print backup_instance('us-east-1', 'i-12345678', instance)
-        except Exception, e: print 'STOP: ', e
-
-        self.assertRaises(Exception, backup_instance, 'us-east-1', 'i-12345678', instance)
+        print "\nTEST 5 - backup.backup_instance(region_name," \
+            " instance_id, instance)"
+        self.assertRaises(Exception, backup_instance, 'us-east-1',
+            'i-12345678', instance)
 
         return
 
-
-    @fudge.patch(test_pkg+'get_region_conn', test_pkg+'delete_broken_snapshots',
-        test_pkg+'_trim_snapshots')
+    @fudge.patch(test_pkg + 'get_region_conn',
+        test_pkg + 'delete_broken_snapshots', test_pkg + '_trim_snapshots')
     def test_trim_snapshots(self, fakeMethod1, fakeMethod2, fakeMethod3):
         fakeMethod1.is_callable().calls(get_region_conn)
         fakeMethod2.is_callable().calls(delete_broken_snapshots)
         fakeMethod3.is_callable().calls(_trim_snapshots)
 
         print "\nTEST 1 - backup.trim_snapshots(region_name)"
-        try: trim_snapshots('us-east-1')
-        except Exception, e: print 'STOP: ', e
+        self.assertIsNone(trim_snapshots('us-east-1'),
+            'Oops, we have catched an error')
 
         print "\nTEST 2 - backup.trim_snapshots()"
-        try: trim_snapshots()
-        except Exception, e: print 'STOP: ', e
+        self.assertIsNone(trim_snapshots(), 'Oops, we have catched an error')
 
         return
 
-
-    @fudge.patch(test_pkg+'get_region_conn', test_pkg+'get_snap_device')
+    @fudge.patch(test_pkg + 'get_region_conn', test_pkg + 'get_snap_device')
     def test_rsync_snapshot(self, fakeMethod1, fakeMethod2):
-        #rsync_snapshot(src_region_name, snapshot_id, dst_region_name, src_inst=None, dst_inst=None)
+        #rsync_snapshot(src_region_name, snapshot_id, dst_region_name,
+        #    src_inst=None, dst_inst=None)
         fakeMethod1.is_callable().calls(get_region_conn)
         fakeMethod2.is_callable().calls(get_snap_device)
 
-        print "\nCALL for backup.rsync_snapshot(src_region_name, snapshot_id, dst_region_name)"
-        try: rsync_snapshot('us-east-1', 'snap-12345678', 'us-west-1')
-        except Exception, e: print 'STOP: ', e
-#        rsync_snapshot('us-east-1', 'snap-12345678', 'us-west-1')
+        print "\nCALL for backup.rsync_snapshot(src_region_name," \
+            " snapshot_id, dst_region_name)"
+        self.assertIsNone(rsync_snapshot('us-east-1', 'snap-12345678',
+            'us-west-1'), 'Oops, we have catched an error')
 
         return
-
-
-# Run testing Tasks
-#print '\nRUN test for backup.backup_instance'
-#test_backup_instance()
-
-#print '\nRUN test for backup.trim_snapshots'
-#test_trim_snapshots()
-
-#print '\nRUN test for backup.rsync_snapshot'
-#test_rsync_snapshot()
 
 
 if __name__ == '__main__':
